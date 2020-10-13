0502ca37bf35d781adde6bab53deda22a6124aeb
Nett <welshnn@outlook.com>
2020-10-13 15:26:15 -0700

    5 test update

--- HW/HW4/test_hangman.py
+++ HW/HW4/test_hangman.py
@@ -1,26 +1,31 @@
-# from hangman import
-
-# def test_print_fn():
-#     assert(print_fn("APPLE", ['A', 'L']) == "A__L_")
-#     assert(print_fn("APPLE", ['A', 'L']) == "A__L_")
+from hangman import game_guess, check_game_over
 
 
-# def test_():
-#     assert(() == )
-#     assert(() == )
-#     assert(() == )
+def test_1():
+    assert(game_guess(['A', 'L'], "APPLE") == "A__L_")
+    assert(game_guess(['A', 'L', 'E'], "APPLE") == "A__LE")
+    assert(game_guess(['T', 'U', 'J', 'X'], "APPLE") == "_____")
+    assert(game_guess(['A', 'P', 'L', 'E'], "APPLE") == "APPLE")
 
-# def test_2():
-#     assert(() == )
-#     assert(() == )
-#     assert(() == )
 
-# def test_3():
-#     assert(() == )
-#     assert(() == )
-#     assert(() == )
+def test_2():
+    assert(game_guess(['O', 'B'], "OBVIOUS") == "OB__O__")
+    assert(game_guess(['O', 'V', 'U'], "OBVIOUS") == "O_V_OU_")
+    assert(game_guess(['O', 'B', 'I', 'S', 'V', 'U'], "OBVIOUS") == "OBVIOUS")
 
-# def test_4():
-#     assert(() == )
-#     assert(() == )
-#     assert(() == )
+
+def test_3():
+    assert(game_guess(['X', 'Y'], "XYLOPHONE") == "XY_______")
+    assert(game_guess(['X', 'Y', 'O'], "XYLOPHONE") == "XY_O__O__")
+
+
+def test_4():
+    assert(check_game_over("APPLE") is True)
+    assert(check_game_over("OBVIOUS") is True)
+    assert(check_game_over("XYOLPHONE") is True)
+
+
+def test_5():
+    assert(check_game_over("_PPL_") is False)
+    assert(check_game_over("OBVIOU_") is False)
+    assert(check_game_over("X_OLPHON_") is False)
