bbde0ca9953909ec6a0be57c2e3d17cd62f4de7f
Nett <welshnn@outlook.com>
2020-10-13 00:01:19 -0700

    fix test cases for hangman

--- HW/HW4/hangman.py
+++ HW/HW4/hangman.py
@@ -26,31 +26,38 @@ def main():
     won = 0
 
     for word in words:
+        won_round = False
         tries = 6
         user_guess = []
-
+        print("_" * len(word))
         while tries > 0:
             user_input = input("Enter a letter or word: ").upper()
             if len(user_input) == 1:
                 if user_input in user_guess:
-                    print("You've already guessed this letter!")
+                    print("You've already guessed that letter!")
+                    hidden_word = test(user_guess, word)
+                    print(hidden_word)
+                    print("Your guesses so far: " + "".join(user_guess))
                     continue
                 tries -= 1
                 user_guess.append(user_input)
                 hidden_word = test(user_guess, word)
-
-                print(hidden_word)
-
                 if check_game_over(hidden_word):
                     won += 1
                     print("You won!")
                     break
+                else:
+                    if tries == 0:
+                        print("You lose! The word was", word)
+                        break
             else:
                 if user_input == word:
                     won += 1
                     print("You win!")
                     break
-            print("Your guesses so far: " + " ".join(user_guess))
+            hidden_word = test(user_guess, word)
+            print(hidden_word)
+            print("Your guesses so far: " + "".join(user_guess))
     print("You won", won, "out of 3")
 
 if __name__ == "__main__":
