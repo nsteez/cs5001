a24d5f70b04eb4521a35086a0e37c655c4c42068
Nett <welshnn@outlook.com>
2020-10-28 16:24:49 -0700

    update

--- HW/HW5/upc.py
+++ HW/HW5/upc.py
@@ -3,25 +3,45 @@ Netti Welsh
 Fall 2020 CS5001
 Problem 1: Universal product code
 This program will take a string as input and return True if
-the supplied string is valid
+the supplied string is a valid universal product code
 """
 
+
 def is_valid_upc(user_input):
+    '''Function --is_valid_upc
+        Reverses string input, checks if user_input is a digit,
+        checks if the sum of is_valid_upc_part2 is divisible by 10.
+    Parameters:
+        user_input -- string input
+    Returns:
+        True -- if sum is divisible by 10
+        False -- otherwise
+    '''
     user_input = user_input[::-1]
-    if user_input.isdigit() == False:
+    if user_input.isdigit() is False:
         return False
-    result = is_valid_upc_part2(user_input)
-    if sum(result) % 10 ==0:
+    result = is_valid_upc_split(user_input)
+    if sum(result) % 10 == 0:
         return True
     else:
         return False
 
-def is_valid_upc_part2(user_input):
+
+def is_valid_upc_split(user_input):
+    '''Function -- is_valid_upc_split
+        Splits odd and even indexes, multiplies the odd index list integer by 3
+        add those values with even indexes integer values
+    Parameters:
+        user_input -- string input
+    Returns:
+        The results of the odd index values list combined with
+        the even index value list
+    '''
     even_list = []
     odd_list = []
     for i in range(0, len(user_input)):
         user_input_list = list(user_input)
-        if i % 2 ==0:
+        if i % 2 == 0:
             even_list.append(user_input_list[i])
         else:
             odd_list.append(user_input_list[i])
@@ -33,6 +53,7 @@ def is_valid_upc_part2(user_input):
 def main():
     user_input = input("Enter upc")
     print(is_valid_upc(user_input))
+
 
 if __name__ == "__main__":
     main()
