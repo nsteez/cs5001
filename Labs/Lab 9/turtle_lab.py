cf773bd20668c1004d7e1fd298cefc082fc69b13
Nett <welshnn@outlook.com>
2020-11-14 19:10:25 -0800

    nested star

--- Labs/Lab 9/turtle_lab.py
+++ Labs/Lab 9/turtle_lab.py
@@ -5,21 +5,29 @@ Lab 9 - Turtle
 """
 import turtle
 
-Screen = turtle.Screen()
-turt = turtle.Turtle() # Create a variable of data type Turtle, with label turt
+def main():
+    DEGREE_ROTATION = 144
+    MAX_SEGMENT_SIZE = 200
+    SEGMENT_INCREASE = 10
+    LAST_LINE = 4
 
-turt.pensize(2)
+    turt = turtle.Turtle()
+    colors = ["red", "yellow", "orange", "pink", "purple"]
+    segment = SEGMENT_INCREASE
+    index = 0
+    turt.pensize(4)
+    while segment < MAX_SEGMENT_SIZE:
+        turt.color(colors[index])
+        turt.forward(segment)
+        turt.right(DEGREE_ROTATION)
+        if(index == LAST_LINE):
+            index = 0
+        else:
+            index+=1
+        segment+=SEGMENT_INCREASE
+
+    turtle.done()
 
 
-circ = ['green','blue','orange','red', 'yellow']
-
-
-for i in range(5):
-    turt.speed(3)
-    turt.pencolor(circ[i])
-    turt.right(288)
-    turt.forward(150)
-    turt.left(144)
-
-
-turtle.done()
+if __name__  == "__main__":
+    main()
