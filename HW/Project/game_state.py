eb7d56d794c14f3603808964a982697bd2629fa1
Nett <welshnn@outlook.com>
2020-12-01 14:45:02 -0800

    added black_king and red_king

--- HW/Project/game_state.py
+++ HW/Project/game_state.py
@@ -1,7 +1,9 @@
 class GameState:
     EMPTY = 'EMPTY'
     BLACK = 'BLACK'
+    BLACK_KING = 'BLACK_KING'
     RED = 'RED'
+    RED_KING = 'RED_KING'
     def __init__(self):
         self.squares = [
             [GameState.EMPTY, GameState.BLACK, GameState.EMPTY, GameState.BLACK, GameState.EMPTY, GameState.BLACK, GameState.EMPTY, GameState.BLACK],
